bd7aec88c6d91237ff56df6cf3154e3ac231b9fa
Dennis Aleynikov <daleynikov@gmail.com>
2018-03-21 16:56:39 -0700

    clean up answer var

--- project/routing.py
+++ project/routing.py
@@ -1,52 +1,53 @@
 import sys
 
-def lookup_list(file):
+def lookup_list(file, cost):
     with open(file) as f:
         content = f.readlines()
 
     for c in content:
-        print(lookup_cost(c))
+        print(lookup_cost(c,cost))
 
 
 
-def lookup_cost(phone):
-    fname = "./data/route-costs-10.txt"
-
+def lookup_cost(phone,fname):
     with open(fname) as f:
         content = f.readlines()
-
-    # create filter
-    reduce = lambda x: phone.startswith(x[:limit]), content
 
     # check first 2 characters
     limit = 2
     # filter by first digit
-    filtered = list(filter(reduce(content)))
+    small = filter(lambda x: x[:limit] in phone, content)
 
     # init answer
     answer = None
 
     # keep incrementing limit until you stop finding a match.
-    while len(filtered) > 1:
-        prefix = prefix + 1
+    while len(list(small)) > 1:
+        # search an extra character
+        limit += 1
 
         # reduce the search area
-        filter_after = list(filter(reduce(filtered)))
-        if( len(filter_after) < 1):
-            answer = filtered[0]
+        smaller = filter(lambda x: x[:limit] in phone, small)
+        if( len(list(smaller)) < 1):
+            answer = small[0]
             break
-        filtered = filter_after
 
-    if answer == None:
-        answer = filtered[0]
+        # get rid of temp list
+        small = smaller
 
+        # set best answer for now
+        answer = list(small)[0]
+
+    # split at comma now
     answer = answer.split(",")
+
+    # make sure it actually matches and isn't a dirty match
     if phone.startswith(answer[0]):
         return answer[1]
     else:
-        return "no route cost"
+        return "not routeable"
 
 
 
 if __name__ == "__main__":
-    lookup_list("./data/phone-numbers-10.txt")
+    lookup_list("./data/phone-numbers-10.txt","./data/route-costs-100.txt")
